745193ec81c5af839fdc18ef2e31a37ab4d83c96
Vladox69 <marwladiagua2001@hotmail.com>
2022-09-08 16:08:31 -0500

    firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraficosComponent } from './graficos/graficos.component';
+import { ReportesComponent } from './reportes/reportes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'reportes',component:ReportesComponent},
+  {path:'graficos',component:GraficosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
